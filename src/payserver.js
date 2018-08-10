c8c2aa1dd2c009b29d83f3a0c77d7fbdd3c3dc79
Parth Mehrotra <parth@mehrotra.me>
2018-08-10 18:54:19 -0400

    qr code done nicely now just need to make prettier

--- src/payserver.js
+++ src/payserver.js
@@ -1,25 +1,31 @@
 import Canvasobject from './canvasobject.js'
+import QRCode from 'qrcode'
 
 export default class Payserver extends Canvasobject {
 
     constructor(matchmakingURL, payserverCallback) {
-        super();
-        this.url = matchmakingURL;
-        this.payserverCallback = payserverCallback;
-        this.progress = 0;
+        super()
+        this.url = matchmakingURL
+        this.payserverCallback = payserverCallback
+        this.progress = 0
     }
 
     getBitcoinAddress() {
-        this.socket = new WebSocket(this.url);
-        this.socket.onmessage = this.payserverMessage.bind(this);
+        this.socket = new WebSocket(this.url)
+        this.socket.onmessage = this.payserverMessage.bind(this)
     }
 
     payserverMessage(e) {
-        let data = JSON.parse(e.data);
-        console.log(data);
+        let data = JSON.parse(e.data)
+        console.log(data)
         if (data['bitcoinAddress']) {
             this.address = data['bitcoinAddress']
-            this.render()
+            QRCode.toDataURL(this.address)
+                .then(url => {
+                    this.dataURL = url
+                    this.render()
+                })
+
         }
         if (data['token']) {
             this.payserverCallback(data['token'])
@@ -27,8 +33,18 @@ export default class Payserver extends Canvasobject {
     }
 
     render() {
-        if (this.address) {
-            QR.draw(this.address, super.getContext().canvas)
+        let ctx = super.getContext()
+        if (this.dataURL) {
+            let img = new Image
+            img.onload = function() {
+                ctx.drawImage(img, 0, 0)
+            }
+            img.src = this.dataURL
         } 
+
+        if (this.address) {
+            ctx.font = "40px Arial"
+            ctx.fillText(this.address, 10, 400)
+        }
     }
 }
