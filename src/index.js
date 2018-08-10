56a84891651c93e38d10a7efd5bfc92bbb0297c6
Matt Bowyer <mattbowyer@Matts-MBP-2.fios-router.home>
2018-08-09 20:27:00 -0400

    added files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import Matchmaker from './matchmaker.js'
 import Gameserver from './gameserver.js'
-// import Payserver from './payserver.js'
+import Payserver from './payserver.js'
 
 class Index {
     constructor() {
-        //this.Payserver = new Payserver("ws://"+window.location.hostname+ ":7000/ws")
-        //this.Payserver.getBitcoinAddress()
+        this.Payserver = new Payserver("ws://"+window.location.hostname+ ":7000/ws")
+        this.Payserver.getBitcoinAddress()
 
-        this.matchmaker = new Matchmaker("ws://" + window.location.hostname + ":8000/ws", this.gameserverReady.bind(this))
-        this.matchmaker.joinQueue()
+        //this.matchmaker = new Matchmaker("ws://" + window.location.hostname + ":8000/ws", this.gameserverReady.bind(this))
+        //this.matchmaker.joinQueue()
     }
 
     gameserverReady(gs) {
--- src/payserver.js
+++ src/payserver.js
@@ -1,4 +1,5 @@
 import Canvasobject from './canvasobject.js'
+import qrcodegen from './assets/qrcodegen.js'
 
 export default class Matchmaker extends Canvasobject {
 
@@ -9,44 +10,31 @@ export default class Matchmaker extends Canvasobject {
         this.progress = 0;
     }
 
-    joinQueue() {
+    getBitcoinAddress() {
         this.socket = new WebSocket(this.url);
-        this.socket.onopen = this.socketOpened.bind(this);
         this.socket.onmessage = this.matchmakingMessage.bind(this);
-    }
-
-    socketOpened() {
-        window.requestAnimationFrame(this.render.bind(this));
     }
 
     matchmakingMessage(e) {
         let data = JSON.parse(e.data);
-        if (data['Port']) {
-            this.gameserverCallback("ws://" + window.location.hostname + ":" + data['Port'] + "/ws");
-        }
-
-        if (data['Status']) {
-            this.updateStatus(data['Status']);
-        }
+        console.log(data);
+        super.getContext().clearRect(0, 0, super.getContext().canvas.width, super.getContext().canvas.height)
+        // Name abbreviated for the sake of these examples her
+        var i = qrcodegen;
+        console.log(i);
+        var j = qrcodegen.qrcode;
+        console.log(j);
+        var ecl = qrcodegen.QrCode.Ecc.HIGH;
+    	var text = "Bitcoin Address";
+    	var segs = qrcodegen.QrSegment.makeSegments(text);
+    	var minVer = 1;
+    	var maxVer = 40;
+    	var mask = -1;
+    	var boostEcc = false;
+        var scale = 4;
+        var border = 4;
+    	var qr = qrcodegen.QrCode.encodeSegments(segs, ecl, minVer, maxVer, mask, boostEcc);
+        qr.drawCanvas(scale, border, canvas);
     }
 
-    updateStatus(s) {
-        this.progress = s.CurrentClients / s.TargetClients;
-        this.render();
-    }
-
-    render() {
-        super.getContext().clearRect(0, 0, super.getContext().canvas.width, super.getContext().canvas.height);
-        super.getContext().beginPath();
-        super.getContext().lineWidth = 3;
-        super.getContext().strokeStyle = 'rgb(108, 116, 128)';
-        super.getContext().arc(
-            super.getContext().canvas.width / 2,
-            super.getContext().canvas.height / 2,
-            100,
-            1.5 * Math.PI,
-            this.progress * 2 * Math.PI + 1.5 * Math.PI
-        );
-        super.getContext().stroke();
-    }
 }
