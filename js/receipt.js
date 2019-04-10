7e33a86c782061fa530bc2baadd5af455fa0c8ec
Matt Bowyer <mattbowyer@Matts-MBP-2.fios-router.home>
2019-04-09 22:31:42 -0400

    finished

--- js/receipt.js
+++ js/receipt.js
@@ -106,7 +106,7 @@ export default class Pay {
             this.modal.appendChild(description);
 
             var link = document.createElement('a');
-            var receipt = document.location.origin + ':9000/token=' + data['token'];
+            var receipt = document.location.origin + '/?token=' + data['token'];
             link.text = receipt;
             link.href = receipt;
             link.target = '_blank';
